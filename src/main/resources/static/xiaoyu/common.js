2a8bbc3b02b34ebe7ed24cd776952c17b018abde
xiaoyu <1546428286@qq.com>
2017-05-15 18:32:38 +0800

    文章列表适应手机端

--- src/main/resources/static/xiaoyu/common.js
+++ src/main/resources/static/xiaoyu/common.js
@@ -225,3 +225,18 @@ function D2Str(time){
     var second = datetime.getSeconds()< 10 ? "0" + datetime.getSeconds() : datetime.getSeconds();
     return year + "-" + month + "-" + date+" "+hour+":"+minute+":"+second;
 }
+
+function isPC() {
+    var userAgentInfo = navigator.userAgent;
+    var Agents = ["Android", "iPhone",
+                "SymbianOS", "Windows Phone",
+                "iPad", "iPod"];
+    var flag = true;
+    for (var v = 0; v < Agents.length; v++) {
+        if (userAgentInfo.indexOf(Agents[v]) > 0) {
+            flag = false;
+            break;
+        }
+    }
+    return flag;
+}
