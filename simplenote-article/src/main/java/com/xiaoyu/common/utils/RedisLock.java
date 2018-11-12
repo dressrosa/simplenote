272be72a552b28bda41d1f18ba7edc2ba202232e
xiaoyu <1546428286@qq.com>
2018-11-12 20:40:57 +0800

    fix redislock

--- simplenote-article/src/main/java/com/xiaoyu/common/utils/RedisLock.java
+++ simplenote-article/src/main/java/com/xiaoyu/common/utils/RedisLock.java
@@ -21,10 +21,11 @@ import redis.clients.jedis.Jedis;
  */
 public class RedisLock {
 
-    private static final int Expire_Seconds = 3600;
+    private static final int Default_Expire_Seconds = 3600;
 
     /**
-     * 加锁数量
+     * 加锁数量.同一个线程内可能有多个锁并列或内嵌存在,因此在每个锁最终释放的时候(即无重入锁),
+     * accquires的值必须为0.
      */
     private static final ThreadLocal<Integer> accquires = new ThreadLocal<Integer>() {
         @Override
@@ -42,7 +43,7 @@ public class RedisLock {
 
     public static RedisLock getRedisLock(String key) {
         if (key == null || "".equals(key)) {
-            return null;
+            throw new RuntimeException("key must not be empty");
         }
         return new RedisLock(key);
     }
@@ -54,10 +55,14 @@ public class RedisLock {
      * @return
      */
     public <T> T lock(Supplier<T> s) {
-        if (tryLock()) {
-            try {
+        boolean locked = false;
+        try {
+            locked = tryLock();
+            if (locked) {
                 return s.get();
-            } finally {
+            }
+        } finally {
+            if (locked) {
                 tryRelease();
             }
         }
@@ -71,10 +76,52 @@ public class RedisLock {
      * @param r
      */
     public void lock(Runnable r) {
-        if (tryLock()) {
-            try {
+        boolean locked = false;
+        try {
+            locked = tryLock();
+            if (locked) {
                 r.run();
-            } finally {
+            }
+        } finally {
+            if (locked) {
+                tryRelease();
+            }
+        }
+    }
+
+    /**
+     * 传入函数,返回执行结果,如果获取锁失败,返回null
+     * 提供一个过期时间,来让调用方更加明确估计自己业务的
+     * 所需时间,来平衡异常所带来的等待时间
+     * 
+     * @param s
+     * @param expireSeconds
+     * @return
+     */
+    public <T> T lock(Supplier<T> s, int expireSeconds) {
+        boolean locked = false;
+        try {
+            locked = tryLock(expireSeconds);
+            if (locked) {
+                return s.get();
+            }
+        } finally {
+            if (locked) {
+                tryRelease();
+            }
+        }
+        return null;
+    }
+
+    public void lock(Runnable r, int expireSeconds) {
+        boolean locked = false;
+        try {
+            locked = tryLock(expireSeconds);
+            if (locked) {
+                r.run();
+            }
+        } finally {
+            if (locked) {
                 tryRelease();
             }
         }
@@ -86,18 +133,31 @@ public class RedisLock {
             accquires.set(n + 1);
             return true;
         }
-        return this.doLock();
+        return this.doLock(0);
     }
 
-    private final boolean doLock() {
+    private final boolean tryLock(int expireSeconds) {
+        int n = accquires.get();
+        if (n > 1) {
+            accquires.set(n + 1);
+            return true;
+        }
+        return this.doLock(expireSeconds);
+    }
+
+    private final boolean doLock(int expireSeconds) {
         int n = accquires.get();
         Jedis jedis = JedisUtils.getRedis();
         try {
-            lockValue = System.currentTimeMillis() + Expire_Seconds * 1000;
+            int seconds = Default_Expire_Seconds;
+            if (expireSeconds > 0) {
+                seconds = expireSeconds;
+            }
+            lockValue = System.currentTimeMillis() + seconds * 1000;
             // 竞争上锁成功
             if (jedis.setnx(key, lockValue + "") == 1) {
                 // 这里可能失败
-                jedis.expire(key, Expire_Seconds);
+                jedis.expire(key, seconds);
                 accquires.set(n + 1);
                 return true;
             }
@@ -106,19 +166,18 @@ public class RedisLock {
                 String current = jedis.get(key);
                 // 上次没有成功设置expire,现在其实已经过期了
                 if (Long.valueOf(current).longValue() < System.currentTimeMillis()) {
-                    lockValue = System.currentTimeMillis() + Expire_Seconds * 1000;
+                    lockValue = System.currentTimeMillis() + seconds * 1000;
                     // 都来竞争上锁
                     String old = jedis.getSet(key, lockValue + "");
                     // 竞争上锁成功
                     if (String.valueOf(current).equals(old)) {
-                        jedis.expire(key, Expire_Seconds);
+                        jedis.expire(key, seconds);
                         accquires.set(n + 1);
                         return true;
                     } else {
                         return false;
                     }
                 }
-
             }
         } finally {
             if (jedis != null) {
@@ -147,6 +206,8 @@ public class RedisLock {
             }
         } finally {
             // 走到这里,说明其他重入锁已经释放,开始第一层加锁的释放.
+            // 这里是必须执行的.因为重入锁是内嵌锁,但是该锁执行完后,可能接下来又立即获取了锁,
+            // 显而易见,这俩个锁是处于同一线程内,如果这里不执行,accquires是等于1的.
             accquires.set(0);
             if (jedis != null) {
                 jedis.close();
